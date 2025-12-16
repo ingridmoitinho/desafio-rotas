a00be7b54480b8a0354b21cea9b87eae4b949d88
ingridmoitinho <ingridmoitinho@gmail.com>
2025-12-16 16:26:49 -0300

    feat: adiciona menu hambúrguer e responsividade em todas as páginas

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,17 +1,30 @@
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
 import s from './header.module.scss'
 import logo from '../../assets/logo.png'
 
 const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false)
+
   return (
     <header className={s.header}>
       <div className={s.boxLogo}>
         <Link to='/'>
-        <img src={logo} alt="Logo coração" />
+          <img src={logo} alt="Logo coração" />
         </Link>
         <Link to='/'>Médicos e Dentistas</Link>
       </div>
-      <nav>
+
+      {/* Botão Hambúrguer */}
+      <button 
+        className={s.hamburger} 
+        onClick={() => setMenuOpen(!menuOpen)}
+        aria-label="Abrir menu"
+      >
+        {menuOpen ? '✕' : '☰'}
+      </button>
+    
+      <nav className={`${s.navDesktop} ${menuOpen ? s.active : ''}`}>
         <Link to='/'>Home</Link>
         <Link to='/voluntario'>Seja Voluntário</Link>
       </nav>
@@ -19,4 +32,4 @@ const Header = () => {
   )
 }
 
-export default Header;
+export default Header
